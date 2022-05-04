648b5705c859691422852cdf79922257420736de
Vincent Adam <vincent.adam@t-online.de>
2022-05-04 16:12:58 +0200

    moved files, changed unified interface, added Helper.h

---
+++ src/include/Filters.h
@@ -0,0 +1,53 @@
+#pragma once
+
+template <typename T>
+class Filter {
+public:
+    Filter(unsigned index, T value) : index(index), value(value) {}
+
+    virtual bool match(T cell) = 0;
+
+public:
+    unsigned index;
+    T value;
+};
+
+template <typename T>
+class Equal : public Filter<T> {
+    using Filter<T>::Filter;
+
+public:
+    bool match(T cell) override { return *cell == this->value; }
+};
+
+template <typename T>
+class LessThan : public Filter<T> {
+    using Filter<T>::Filter;
+
+public:
+    bool match(T cell) override { return *cell < this->value; }
+};
+
+template <typename T>
+class LessEqual : public Filter<T> {
+    using Filter<T>::Filter;
+
+public:
+    bool match(T cell) override { return *cell <= this->value; }
+};
+
+template <typename T>
+class GreaterThan : public Filter<T> {
+    using Filter<T>::Filter;
+
+public:
+    bool match(T cell) override { return *cell > this->value; }
+};
+
+template <typename T>
+class GreaterEqual : public Filter<T> {
+    using Filter<T>::Filter;
+
+public:
+    bool match(T cell) override { return *cell >= this->value; }
+};
