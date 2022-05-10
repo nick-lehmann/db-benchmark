10dcaa6af67372484c23e94a46f9f9fb1bf37b10
eric-stange <Eric.Stange@mailbox.tu-dresden.de>
2022-05-10 21:31:10 +0200

    First version of RowStore::BaseTable
    - add first version of BaseTable
    - not quite complete and not fully working

--- src/row_store/main.cpp
+++ src/row_store/main.cpp
@@ -6,27 +6,36 @@
 #include "Table.h"
 #include "Projection.h"
 #include "Filter.h"
+#include "BaseTable.h"
+#include "Helper.h"
 
 int main(int argc, char ** argv) {
     using Type = uint64_t;
 
     std::cout << "Row-Store Code" << std::endl;
-    auto baseTable = createSortedTestTable<Type>(5, 100, 50);
+    const int** initialData = TableHelper::generateRandomData<int>(5, 20, 1, 10);
+    RowStore::BaseTable<int> baseTable(5, 20, initialData);
 
-    baseTable->print();
+    std::cout << "Print Test-BaseTable: \n" << std::endl;
+    baseTable.print();
 
     std::vector<int> projectionAttributes = {0, 2, 3};
     std::vector<Filter<Type> *> filters;
     filters.push_back(new LessThan<Type>(2, 42));
 
+    /* commented out until implementation of baseTable & IntermediateTable is complete and working
     auto result = baseTable->query(projectionAttributes, filters);
 
     result->print();
+    */
+
 
     //destruct tables -> free allocated memory
     delete baseTable;
+    
+    /* commented out until implementation of baseTable & IntermediateTable is complete and working
     delete result;
-
+    */
 
 	return 0;
 }
