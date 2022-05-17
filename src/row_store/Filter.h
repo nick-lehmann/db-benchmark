b61419ec766830b7441fe916ffa3f8e649c1632a
cornzz <39997278+cornzz@users.noreply.github.com>
2022-05-17 12:00:44 +0200

    Apply all filters on row at once, fix memory leak

--- src/row_store/Filter.h
+++ src/row_store/Filter.h
@@ -5,38 +5,33 @@
 #include <stdexcept>
 
 namespace RowStore {
-template <typename T> IntermediateTable<T> *apply_filter(IntermediateTable<T> &table, Filter<T> *filter) {
-  if (filter->index < 0 || filter->index >= table.getTupleWidth()) {
-    throw std::invalid_argument("Error! Invalid attribute index!");
+template <typename T>
+IntermediateTable<T> *apply_filters(IntermediateTable<T> &table, std::vector<Filter<T> *> &filters) {
+  // Validate filters
+  for (int i = 0; i < filters.size(); ++i) {
+    if (filters[i]->index < 0 || filters[i]->index >= table.getTupleWidth()) {
+      throw std::invalid_argument("Error! Invalid attribute index!");
+    }
   }
-
   // Get pointer to table data
-  auto data = table.getData();
-
+  std::vector<T *> *data = table.getData();
   // Create empty intermediate table
   auto result = new IntermediateTable<T>(table.getTupleWidth());
 
   // Iterate over table rows
   for (int i = 0; i < table.count(); ++i) {
-    // Add matching rows to result table
-    if (filter->match((*data)[i][filter->index])) {
+    bool match = true;
+    // Iterate over filters and match
+    for (int j = 0; j < filters.size(); ++j) {
+      if (!filters[j]->match((*data)[i][filters[j]->index])) {
+        match = false;
+      }
+    }
+    if (match) {
       result->addRow((*data)[i]);
     }
   }
 
   return result;
 }
-
-template <typename T>
-IntermediateTable<T> *apply_filters(IntermediateTable<T> &table, std::vector<Filter<T> *> &filters) {
-  // Create empty intermediate table
-  auto result = new IntermediateTable<T>(table.getTupleWidth(), (*table.getData()));
-
-  for (int i = 0; i < filters.size(); i++) {
-    result = apply_filter((*result), filters[i]);
-  }
-
-  return result;
-}
-
 } // namespace RowStore
