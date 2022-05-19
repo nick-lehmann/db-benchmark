2cfa57bd0208fd8081840d4280e54efac7ef8275
tcq1 <chu.trung@web.de>
2022-05-19 13:27:40 +0000

    include real time as well

--- src/include/Benchmark.h
+++ src/include/Benchmark.h
@@ -1,6 +1,7 @@
 #include <chrono>
 #include <iostream>
 #include <string.h>
+#include <tuple>
 #include <vector>
 
 template <typename T> class Benchmark {
@@ -8,14 +9,24 @@ template <typename T> class Benchmark {
 public:
   Benchmark() = default;
 
-  uint64_t measureTime(Table<T> *table, std::vector<unsigned> &projection,
-                       std::vector<Filter<T> *> &filters) {
-    auto startTime = std::clock();
+  uint64_t measureTime(Table<T> *table, std::vector<unsigned> &projection, std::vector<Filter<T> *> &filters) {
+    // measure both cpu time and real time
+    auto clockStartTime = std::clock();
+    auto realStartTime = std::chrono::steady_clock::now();
+
+    // call query count function
     uint64_t count = table->query_count(projection, filters);
-    auto endTime = std::clock();
-    std::cout << std::fixed << std::setprecision(4)
-              << "Query time: " << 1000 * (endTime - startTime) / CLOCKS_PER_SEC
-              << " ms" << std::endl;
+
+    auto clockEndTime = std::clock();
+    auto realEndTime = std::chrono::steady_clock::now();
+
+    // print result
+    std::cout << std::fixed << std::setprecision(8)
+              << "Query time: " << 1000 * (clockEndTime - clockStartTime) / CLOCKS_PER_SEC << " ms" << std::endl
+              << "Real time: " << std::chrono::duration<double, std::milli>(realEndTime - realStartTime).count()
+              << " ms" << std::endl
+              << "Row Count: " << count << std::endl;
+
     return count;
   }
 };
