6b9e1549ad750a792a19c69089b90bdeed6239ff
Vincent Adam <vincent.adam@t-online.de>
2022-05-31 11:59:48 +0200

    Added AVX-enabled version of column store for testing purposes

--- src/column_store/ColumnStoreTable.h
+++ src/column_store/ColumnStoreTable.h
@@ -3,46 +3,47 @@
 #include <algorithm>
 #include <vector>
 
-#include "Table.h"
+#include "ITable.h"
 
-namespace ColumnStore {
+namespace ColumnStore::Basic {
     template<typename T>
-    class ColumnStoreTable : public Table<T> {
+    class Table : public Tables::Basic::ITable<T> {
     public:
-        ColumnStoreTable(unsigned numAttributes, unsigned numRows, const T **initialData)
-                : Table<T>(numAttributes, numRows, initialData) {
+        Table(uint64_t numAttributes, uint64_t numRows, const T **initialData)
+                : Tables::Basic::ITable<T>(numAttributes, numRows, initialData) {
             data = new std::vector<T>[numAttributes];
-            for (unsigned row = 0; row < numRows; row++) {
-                for (unsigned column = 0; column < numAttributes; column++) {
+            for (uint64_t row = 0; row < numRows; row++) {
+                for (uint64_t column = 0; column < numAttributes; column++) {
                     data[column].push_back(initialData[row][column]);
                 }
             }
         }
 
-        ~ColumnStoreTable() override = default;
+        ~Table() override = default;
 
-        T *getRow(unsigned rowIndex) override {
+        T *getRow(uint64_t rowIndex) override {
             auto returnRow = new T[this->numberOfAttributes];
-            for (unsigned i = 0; i < this->numberOfAttributes; i++) {
+            for (uint64_t i = 0; i < this->numberOfAttributes; i++) {
                 returnRow[i] = data[i][rowIndex];
             }
             return returnRow;
         }
 
-        std::tuple<T **, unsigned, unsigned>
-        query_table(std::vector<unsigned> &projection, std::vector<Filter<T> *> &filters) override {
-            std::vector<unsigned> *filter_indices = nullptr;
+        std::tuple<T **, uint64_t, uint64_t>
+        query_table(std::vector<uint64_t> &projection, std::vector<Filters::Basic::Filter<T> *> &filters) override {
+            std::vector<uint64_t> *filter_indices = nullptr;
             for (const auto &filter: filters) {
                 filter_indices = filter_basic(filter, filter_indices);
             }
 
             return std::make_tuple(reconstruct_table(projection, filter_indices),
-                                   (unsigned) filter_indices->size(), (unsigned) projection.size());
+                                   (uint64_t) filter_indices->size(), (uint64_t) projection.size());
         }
 
-        uint64_t query_count(std::vector<unsigned> &projection, std::vector<Filter<T> *> &filters) override {
+        uint64_t
+        query_count(std::vector<uint64_t> &projection, std::vector<Filters::Basic::Filter<T> *> &filters) override {
             // the first list of filtered indices is empty
-            std::vector<unsigned> *filter_indices = nullptr;
+            std::vector<uint64_t> *filter_indices = nullptr;
 
             // apply each filter, (hopefully) reducing the index vector each iteration
             for (const auto &filter: filters) {
@@ -55,7 +56,7 @@ namespace ColumnStore {
                 auto access = final_result[filter_indices->size() - 1][projection.size() - 1];
             }
 
-            return (unsigned) filter_indices->size();
+            return (uint64_t) filter_indices->size();
         }
 
     private:
@@ -63,8 +64,9 @@ namespace ColumnStore {
         /// \param filter the filter to apply
         /// \param input_indices the vector of indices that are still in the filtering. This can be a nullptr in which case the entire set of data is accessed.
         /// \return a (reduced) vector of indices for rows that match this filter
-        std::vector<unsigned> *filter_basic(const Filter<T> *filter, std::vector<unsigned> *input_indices = nullptr) {
-            auto output_indices = new std::vector<unsigned>();
+        std::vector<uint64_t> *
+        filter_basic(const Filters::Basic::Filter<T> *filter, std::vector<uint64_t> *input_indices = nullptr) {
+            auto output_indices = new std::vector<uint64_t>();
             auto filter_col = data[filter->index];
 
             if (input_indices) { // input_indices is not empty
@@ -73,7 +75,7 @@ namespace ColumnStore {
                         output_indices->push_back(row);
                 }
             } else { // input_indices is empty, iterate over the entire set of data
-                for (unsigned row = 0; row < this->numberOfRows; row++) {
+                for (uint64_t row = 0; row < this->numberOfRows; row++) {
                     if (filter->match(filter_col.at(row)))
                         output_indices->push_back(row);
                 }
@@ -87,13 +89,13 @@ namespace ColumnStore {
         /// Constructs and returns a table (2-dimensional C-array) based on prior filtering and using projection.
         /// \param projection the columns to pack into the table
         /// \param row_indices the rows to pack into the table
-        T **reconstruct_table(std::vector<unsigned> &projection, std::vector<unsigned> *row_indices) {
+        T **reconstruct_table(std::vector<uint64_t> &projection, std::vector<uint64_t> *row_indices) {
             T **to_return = (T **) malloc(row_indices->size() * sizeof(T *));
 
-            for (unsigned row = 0; row < row_indices->size(); row++) {
+            for (uint64_t row = 0; row < row_indices->size(); row++) {
                 T *rowData = (T *) malloc(projection.size() * sizeof(T));
                 to_return[row] = rowData;
-                for (unsigned projection_index = 0; projection_index < projection.size(); projection_index++) {
+                for (uint64_t projection_index = 0; projection_index < projection.size(); projection_index++) {
                     to_return[row][projection_index] = data[projection.at(projection_index)][row_indices->at(row)];
                 }
             }
--- src/column_store/main.cpp
+++ src/column_store/main.cpp
@@ -1,24 +1,49 @@
 #include <iostream>
 
 #include "ColumnStoreTable.h"
+#include "ColumnStoreTable_AVX.h"
+
 #include "Helper.h"
 
 #define COLUMNS 5
-#define ROWS 20
+#define ROWS 16
+
+// taken from https://www.tutorialspoint.com/cplusplus-equivalent-of-instanceof
+template<typename Base, typename T>
+inline bool instanceof(const T *ptr) {
+    return dynamic_cast<const Base*>(ptr) != nullptr;
+}
+
+/// Small helper function for debugging output. It is not very pretty, it will be removed sooner or later.
+template <typename T>
+void printFilterInfo(std::vector<Filters::AVX::Filter<T>*> filters) {
+    std::cout << "Filters ";
+    for (auto i = 0; i < filters.size(); i++) {
+        std::cout << "(" << filters.at(i)->index << ", ";
+        if (instanceof<Filters::AVX::Equal<T>>(filters.at(i))) std::cout << "==";
+        else if (instanceof<Filters::AVX::NotEqual<T>>(filters.at(i))) std::cout << "!=";
+        else if (instanceof<Filters::AVX::GreaterThan<T>>(filters.at(i))) std::cout << " >";
+        else if (instanceof<Filters::AVX::GreaterEqual<T>>(filters.at(i))) std::cout << ">=";
+        else if (instanceof<Filters::AVX::LessThan<T>>(filters.at(i))) std::cout << " <";
+        else if (instanceof<Filters::AVX::LessEqual<T>>(filters.at(i))) std::cout << "<=";
+        std::cout << filters.at(i)->value << ")" << (i == filters.size() - 2 ? " and " : (i < filters.size() - 2 ? ", " : ""));
+    }
+    std::cout << std::endl;
+}
 
 void debugTest() {
     const int** initialData = TableHelper::generateRandomData<int>(COLUMNS, ROWS, 1, 10);
-    ColumnStore::ColumnStoreTable<int> testTable(COLUMNS, ROWS, initialData);
+    ColumnStore::Basic::Table<int> testTable(COLUMNS, ROWS, initialData);
 
     std::cout << "Test table: \n" << std::endl;
 
     testTable.print();
 
-    auto equalFilter = new Equal<int>(0, 4);
-    auto greaterFilter = new GreaterThan<int>(4, 4);
+    auto equalFilter = new Filters::Basic::Equal<int>(0, 4);
+    auto greaterFilter = new Filters::Basic::GreaterThan<int>(4, 4);
 
-    std::vector<Filter<int>*> filters {equalFilter, greaterFilter};
-    std::vector<unsigned> projection {0, 2, 4};
+    std::vector<Filters::Basic::Filter<int>*> filters {equalFilter, greaterFilter};
+    std::vector<uint64_t> projection {0, 2, 4};
 
     auto [queried, rows, columns] = testTable.query_table(projection, filters);
 
@@ -29,7 +54,29 @@ void debugTest() {
     std::cout << "Number of rows: " << testTable.query_count(projection, filters) << std::endl;
 }
 
+template <typename T>
+void avx_test() {
+    const T** initialData = TableHelper::generateRandomData<T>(COLUMNS, ROWS, 1, 5);
+    ColumnStore::AVX::Table<T> testTable(COLUMNS, ROWS, initialData);
+
+    testTable.print();
+
+    auto equalFilter = new Filters::AVX::GreaterThan<T>(0, 2);
+    auto equalFilter2 = new Filters::AVX::NotEqual<T>(1, 3);
+    auto equalFilter3 = new Filters::AVX::Equal<T>(3, 1);
+
+    std::vector<Filters::AVX::Filter<T>*> filters {equalFilter, equalFilter2, equalFilter3};
+    std::vector<uint64_t> projection {0, 1, 2, 3, 4};
+
+    auto [queried, rows, columns] = testTable.query_table(projection, filters);
+
+    printFilterInfo<T>(filters);
+    std::cout << std::endl;
+
+    TableHelper::printTable(queried, columns, rows);
+}
+
 int main(int argc, char ** argv) {
-    debugTest();
+    avx_test<uint32_t>();
     return 0;
 }
