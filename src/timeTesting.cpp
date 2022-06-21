519f0354ea8c3a894c003d5fb07dab8fc063b9ef
s1787956 <tim.lenz@tu-dresden.de>
2022-06-21 12:04:06 +0000

    tried running the first AVX-benchmark

--- src/timeTesting.cpp
+++ src/timeTesting.cpp
@@ -25,9 +25,9 @@ int main(int argc, char ** argv) {
 
     using Type = uint64_t;
 
-    std::vector<Filters::Filter<Type, SIMD::AVX512>*> filters  {new Filters::Equal<Type, SIMD::AVX512> (1, 1),new Filters::GreaterThan<Type, SIMD::AVX512> (0,31),new Filters::LessThan<Type, SIMD::AVX512> (3,76),
+    std::vector<Filters::Filter<Type, SIMD::AVX512>*> filters  {new Filters::Equal<Type, SIMD::AVX512> (1, 20),new Filters::GreaterThan<Type, SIMD::AVX512> (5,31),new Filters::LessThan<Type, SIMD::AVX512> (3,76),
                                                                  new Filters::Equal<Type, SIMD::AVX512> (4,42),new Filters::GreaterThan<Type, SIMD::AVX512> (8,42),new Filters::LessEqual<Type, SIMD::AVX512> (6,56)};
-    std::vector<Type> projection {0,1,2,4,8};
+    std::vector<Type> projection {1,2,3,4,5,6,8};
 
     //std::vector<Filter::Filter<Type, SIMD::AVX512> *> filters{gtAVX,eqAVX,ltAVX,
     //                                                          eqAVX2,gtAVX2,ltAVX2};
@@ -44,11 +44,11 @@ int main(int argc, char ** argv) {
 
     Benchmark::runBenchmark<uint64_t>(2,projection,filters,10,10,false,50,300,1,
                             1000,0,"PSbenchmark.csv");*/
-    Benchmark::benchmarkRows<Type>(0,projection,filters,100,50,false,100,250,0,
-                                    100,42,"../output_files/RSbenchmark.csv");
-    Benchmark::benchmarkRows<Type>(1,projection,filters,100,50,false,100,250,0,
-                                      100,42,"../output_files/CSbenchmark.csv");
-    Benchmark::benchmarkRows<Type>(2,projection,filters,100,50,false,100,250,0,
+    //Benchmark::benchmarkRows<Type>(0,projection,filters,100,50,false,100,250,0,
+    //                                100,42,"../output_files/RSbenchmark.csv");
+    Benchmark::benchmarkRows<Type>(1,projection,filters,100,50,false,100,250,1,
+                                     100,42,"../output_files/CSbenchmark.csv");
+    Benchmark::benchmarkRows<Type>(2,projection,filters,100,50,false,100,250,1,
                                    100,42,"../output_files/PSbenchmark.csv");
 
 
