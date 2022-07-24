760a19ad697e5cfb3db1cfb7f510e6c01657c4aa
s1787956--tu-dresden.de <tim.lenz@tu-dresden.de>
2022-07-24 17:04:57 +0200

    fixing some more errors..

--- src/timeTesting.cpp
+++ src/timeTesting.cpp
@@ -10,7 +10,6 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <format>
 #include "Benchmark.h"
 
 //#include "Table.h"
@@ -24,11 +23,11 @@ void benchmark(std::vector<uint64_t> &projection,
               std::vector<Filters::Filter<T, Variant> *> &filters, const std::string &fileId) {
 
     Benchmark::benchmarkRows<T,Variant>(2,projection,filters_AVX,100,50,false,100,500,0,
-                                                   100,42,std::format("../output_files/PSbenchmark_{}.csv", fileId));
+                                                   100,42,"../output_files/PSbenchmark_" + fileId +".csv");
     Benchmark::benchmarkRows<T,Variant>(1,projection,filters_AVX,100,50,false,100,500,0,
-                                                   100,42,std::format("../output_files/CSbenchmark_{}.csv", fileId));
+                                                   100,42,"../output_files/CSbenchmark_" + fileId +".csv");
     Benchmark::benchmarkRows<T,Variant>(0,projection,filters_AVX,100,50,false,100,500,0,
-                                                   100,42,std::format("../output_files/RSbenchmark_{}.csv", fileId));
+                                                   100,42,std::format("../output_files/RSbenchmark_" + fileId +".csv");
 
 }
 
